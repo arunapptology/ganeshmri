1f42f6b4dc7f89a95bf4a6fa46e323fb21ce5673
Arun <arun.outlook1992@gmail.com>
2023-10-10 18:43:36 +0530

    19 commit

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
         ignoreDuringBuilds: true
      }, 
      env: {
-        BASE_URL: 'http://localhost:3000/',
+        BASE_URL: 'https://ganeshmri.com/',
       },
     async headers() {
         return [
