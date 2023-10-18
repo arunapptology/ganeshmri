180079d92ecf91b8bc1bd0991db887907a5f5636
Arun <arun.outlook1992@gmail.com>
2023-10-18 14:55:11 +0530

    25 commit

---
+++ src/app/GoogleTagManager.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import  Script  from 'next/script';
+
+  
+const GoogleTagManager = () => {
+  return (
+    <>
+
+
+
+
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-PRBF8ZEVYX');
+        `}
+      </Script>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-PRBF8ZEVYX" />
+
+
+
+
+    </>
+  )
+}
+
+export default GoogleTagManager
